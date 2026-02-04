63efa4f29eaea8d308ccef93e0f2eab4699d7448
Ajano <aleksandar@laravel.com>
2026-02-04 21:24:00 +0100

    Add gallery functionality with controllers and models2

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -4,20 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use App\Models\ProjectImage;
+use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
-    // Display all images from all projects
     public function index()
     {
-        $images = ProjectImage::with('project')
-            ->latest()
-            ->paginate(12);
-
-        return view('Gallery.index', compact('images'));
+        $images = ProjectImage::with('project')->latest()->paginate(12);
+        return view('gallery.index', compact('images'));
     }
 
-    // Display images for a specific project
     public function projectGallery($projectId)
     {
-        $project = Project::with('images')->findOrFail($project
+        $project = Project::with('images')->findOrFail($projectId);
+        return view('gallery.project', compact('project'));
+    }
+}
