c211ff53a5cdb32e1e946ef3a8ad9bb5e6c0de5d
Ajano <aleksandar@laravel.com>
2026-02-05 11:28:48 +0100

    Force view update - fix gallery route name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -197,3 +197,10 @@
     </footer>
 </body>
 </html>
+<ul class="nav-links">
+    <li><a href="{{ route('home') }}">Home</a></li>
+    <li><a href="{{ route('fencing.about') }}">About HEMA</a></li>
+    <li><a href="{{ route('fencing.techniques') }}">Techniques</a></li>
+    <li><a href="{{ route('fencing.equipment') }}">Equipment</a></li>
+    <li><a href="{{ route('gallery.index') }}">Gallery</a></li> <!-- fixed -->
+</ul>
